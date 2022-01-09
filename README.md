d5813db7dd9e8800d5238ebc8fcfe665e3ee2fe2
Blaise1030 <61928648+Blaise1030@users.noreply.github.com>
2022-01-10 01:01:57 +0800

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# react-controls
+# React-Controls-Statements
+
+The statement below renders 3 `ELSE IF 1`. Works like normal if else controls.
+
+```
+    <IF c={false}>
+        <div>IF</div>
+        <div>IF</div>
+        <div>IF</div>
+    <ELSEIF c={true} />
+        <div>ELSE IF 1</div>
+        <div>ELSE IF 1</div>
+        <div>ELSE IF 1</div>
+    <ELSEIF c={true} />
+        <div>ELSE IF 2</div>
+        <div>ELSE IF 2</div>
+        <div>ELSE IF 2</div>
+    <ELSE />
+        <div>ELSE</div>
+        <div>ELSE</div>
+        <div>ELSE</div>
+    </IF>
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import { useState } from "react";
-import "./App.css";
+import { ELSE, IF } from "./lib";
+import { ELSEIF } from "./lib/Controls";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div></div>;
+  return (
+    <div>
+      <IF c={false}>
+        <div>IF</div>
+        <div>IF</div>
+        <div>IF</div>
+        <ELSEIF c={true} />
+        <div>ELSE IF 1</div>
+        <div>ELSE IF 1</div>
+        <div>ELSE IF 1</div>
+        <ELSEIF c={true} />
+        <div>ELSE IF 2</div>
+        <div>ELSE IF 2</div>
+        <div>ELSE IF 2</div>
+        <ELSE />
+        <div>ELSE</div>
+        <div>ELSE</div>
+        <div>ELSE</div>
+      </IF>
+    </div>
+  );
 }
 
 export default App;
